cd33b0ce031d7e77b2e3d06aefc77f9d8756fb67
luisdev20 <lescobedo.im@gmail.com>
2021-12-22 23:57:40 -0500

    pycodestyle passed

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,10 @@ class BaseModel:
     """A base class for all hbnb models"""
     if models.engineMode == "db":
         id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-        updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+        created_at = Column(DateTime, default=datetime.utcnow(),
+                            nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow(),
+                            nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
